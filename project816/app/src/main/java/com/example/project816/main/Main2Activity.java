e6290f673923128a37f8f40623515d66ec39dc8f
x_wlgml <kim76955@gmail.com>
2020-09-01 22:56:13 +0900

    구글 로그인 연동 완료. <fragment_setting> 창 오류.

--- project816/app/src/main/java/com/example/project816/main/Main2Activity.java
+++ project816/app/src/main/java/com/example/project816/main/Main2Activity.java
@@ -1,7 +1,12 @@
 package com.example.project816.main;
 
+import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
 import com.example.project816.R;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
@@ -23,10 +28,11 @@ public class Main2Activity extends AppCompatActivity {
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.navigation_home, R.id.navigation_make).build();
+                R.id.navigation_home, R.id.navigation_make, R.id.navigation_setting).build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(navView, navController);
+
 
     }
 
